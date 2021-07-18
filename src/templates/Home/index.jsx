2c494b6df52c5d61e4aa65ab085cd14f1bf56216
Mateus Farias <mateus.f.silva@outlook.com>
2021-07-18 19:06:47 -0300

    exemplo terminado

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,7 +1,34 @@
+import { useCounterContext } from '../../contexts/CounterContext';
+import { Button } from '../../components/Button';
+import { Heading } from '../../components/Heading';
+
 export const Home = () => {
+  const [state, actions] = useCounterContext();
+
+  const handleError = () => {
+    actions
+      .asyncError()
+      .then((r) => console.log(r))
+      .catch((e) => console.log(e.name, ':', e.message));
+  };
+
   return (
     <div>
-      <h1>Hello World!</h1>
+      <Heading />
+
+      <div>
+        <Button onButtonClick={actions.increase}>Increase</Button>
+        <Button onButtonClick={actions.decrease}>Decrease</Button>
+        <Button onButtonClick={actions.reset}>Reset</Button>
+        <Button onButtonClick={() => actions.setCounter({ counter: 10 })}>set 10</Button>
+        <Button onButtonClick={() => actions.setCounter({ counter: 100 })}>set 100</Button>
+        <Button disabled={state.loading} onButtonClick={actions.asyncIncrease}>
+          Async increase
+        </Button>
+        <Button disabled={state.loading} onButtonClick={handleError}>
+          Async error
+        </Button>
+      </div>
     </div>
   );
 };
